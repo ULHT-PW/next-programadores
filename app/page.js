8cacfc3502ef4ec999cc4eaec913329ae160a5ba
Lucio Studer Ferreira <42048382+luciostuder@users.noreply.github.com>
2024-07-05 14:03:19 +0100

    aplicacao next.js simples

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <h2>Bem-vindo à Página dos Programadores</h2>
+      <p>Esta é a página inicial da aplicação.</p>         
+      <p>Apresentamos programadores famosos.</p>      
+    </div>
+  )
+}
+
